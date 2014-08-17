037e745f6b9d339d3e1e789eb87b920f50bbc04e
luochengleo <luochengleo@gmail.com>
2014-08-17 21:00:22 +0800

    add

---
+++ english/src/ForFLSevaluation.py
@@ -0,0 +1,103 @@
+#coding=utf8
+
+from collections import defaultdict
+import os
+import csv
+
+def sort_by_value(d):
+    items = d.items()
+    backitems = [[v[1] , v[0]] for v in items]
+    backitems.sort(reverse=True)
+    return [backitems[i][1] for i in range(0,len(backitems))]
+
+def loadcsv(filename):
+    return csv.reader(open(filename))
+
+def evaid():
+    rtr = []
+    for i in range(51,84,1):
+        if i <10:
+            rtr.append('000'+str(i))
+        else:
+            rtr.append('00'+str(i))
+    return rtr
+
+if 'flseva' not in os.listdir('../data'):
+    os.mkdir('../data/flseva')
+
+####################################################
+userfls2annofls = defaultdict(lambda:'')
+for l in loadcsv('../data/csv/task6.csv'):
+    userfls2annofls[l[1]] = l[3]
+
+####################################################
+annofls2idx = dict()
+id = ''
+count = 1
+iprob = open('../data/flseva/imine.Iprob','w')
+for l in open('../data/temp/flsposs.txt').readlines():
+    segs=  l.strip().split('\t')
+    annofls = segs[1]
+    if segs[0] != id:
+        count = 1
+        id = segs[0]
+    else:
+        count +=1
+    annofls2idx[annofls] = count
+    iprob.write(id+' '+str(count)+' '+segs[2]+'\n')
+######################################################
+run1 = open('../data/flseva/run1.sh','w')
+runlist = open('../data/flseva/iminerunlist','w')
+run2 = open('../data/flseva/run2.sh','w')
+run2.write('cat ./iminerunlist | TRECsplitruns ./imine.Iprob.tid')
+run2.close()
+
+run3 = open('../data/flseva/run3.sh','w')
+
+
+for f in os.listdir('../data/enrun'):
+    run1.write('./DIN-splitqrels imine.Iprob '+f.replace('txt','Dqrels')+' imine'+'\n')
+    runlist.write(f.replace('txt')+'\n')
+    
+    print f
+    dreq = open('../data/flseva/'+f.replace('txt','Dqrels'),'w')
+    alreadyin = set()
+    pairlist = list()
+    for l in open('../data/enrun/'+f).readlines()[1:]:
+        segs = l.strip().replace('&amp;','').split(';')
+        id = segs[0]
+        userfls = segs[2]
+        flsrank = int(segs[3])
+        if (id,userfls,flsrank) not in alreadyin:
+            alreadyin.add((id,userfls,flsrank))
+            pairlist.append((id,userfls,flsrank))
+    
+    for p in pairlist :
+        id = p[0]
+        if id in evaid():
+            
+            userfls = p[1]
+            flsrank = p[2]
+            annofls = userfls2annofls[userfls]
+#             print id,userfls,flsrank,annofls
+
+            if annofls !='':
+                try:
+                    intent = annofls2idx[annofls]
+                except:
+                    print '-------except-------'
+                    print userfls
+                    print annofls
+                    bug = open('../data/out.debug','w')
+                    bug.write('fail key '+annofls+'\n')
+                    for item in annofls2idx.keys():
+                        bug.write(item+'\n')
+                    bug.close()
+                    itent = 0
+                rel = 'L1'
+            else:
+                intent = 0
+                rel = 'L0'
+            dreq.write(id+' '+str(intent)+' '+userfls.replace(' ','')+' '+rel+'\n')
+    dreq.close()
+    
---
+++ english/src/NormailizeSlsWeight.py
@@ -0,0 +1,41 @@
+#coding=utf8
+import csv
+def loadcsv(filename):
+    return csv.reader(open(filename))
+
+def sort_by_value(d):
+    items = d.items()
+    backitems = [[v[1] , v[0]] for v in items]
+    backitems.sort(reverse=True)
+    return [backitems[i][1] for i in range(0,len(backitems))]
+
+from  collections import defaultdict
+
+id2sum =defaultdict(lambda: 0)
+def evaid():
+    rtr = []
+    for i in range(51,83,1):
+        if i <10:
+            rtr.append('000'+str(i))
+        else:
+            rtr.append('00'+str(i))
+    return rtr
+for l in loadcsv('../data/csv/task7.csv'):
+    id = l[0]
+    annosls = l[1]
+    weight = l[5]
+    id2sum[id]+= int(weight)
+
+id2sls2poss = defaultdict(lambda : dict())
+for l in loadcsv('../data/csv/task7.csv'):
+    id = l[0]
+    annosls = l[1]
+    weight = l[5]
+    id2sls2poss[id][annosls] = float(int(weight))/float(id2sum[id])
+
+fout = open('../data/temp/slsposs.txt','w')
+for id in evaid():
+    for item in sort_by_value(id2sls2poss[id]):
+        fout.write(id+'\t'+item+'\t'+str(id2sls2poss[id][item])+'\n')
+
+fout.close()
