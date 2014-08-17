80e08d362e01f50b3add04c61c54894ee3d5b38b
luochengleo <luochengleo@gmail.com>
2014-08-18 02:39:21 +0800

    fa

--- english/src/MergeEvaluate.py
+++ english/src/MergeEvaluate.py
@@ -3,7 +3,10 @@ import os
 import sys
 dir = sys.argv[1]
 cutoff = sys.argv[2]
-for f in os.listdir('../data/Dqrels/sakai/Dnev/'):
+outfile = sys.argv[3]
+fout = open(outfile,'w')
+
+for f in os.listdir(dir):
     Dsharpsum = 0.0
     Dsharpcount = 0.0
     IRelnsum = 0.0
@@ -11,8 +14,7 @@ for f in os.listdir('../data/Dqrels/sakai/Dnev/'):
     
     IRel50sum = 0.0
     IRel50count = 0.0
-
-    for l in open('../data/Dqrels/sakai/Dnev/'+f):
+    for l in open(dir+'/'+f):
         segs = l.strip().split(' ')
         if segs[1] == 'nDCG@'+cutoff+'=':
             for item in segs[2:]:
@@ -30,4 +32,7 @@ for f in os.listdir('../data/Dqrels/sakai/Dnev/'):
                 if item != '':
                     IRel50sum += float(item)
             IRel50count +=1
-    print f.split('.')[0],'\t',Dsharpsum,'\t',Dsharpcount,'\t', IRelnsum,'\t',IRelncount,'\t',IRel50sum,'\t',IRel50count     
+    
+    print f.split('.')[0],'\t',Dsharpsum,'\t',Dsharpcount,'\t', IRelnsum,'\t',IRelncount,'\t',IRel50sum,'\t',IRel50count
+    outfile.write(f.split('.')[0]+','+str(Dsharpsum)+","+str(Dsharpcount)+','+str(IRelnsum)+','+str(IRelncount)+','+str(IRel50sum)+','+str(IRel50count)+'\n')
+outfile.close()    
