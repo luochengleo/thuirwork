5a366f891a2c1556d4e8e607d7afca46c6da832e
luochengleo <luochengleo@gmail.com>
2014-08-17 22:46:32 +0800

    ad

--- english/src/ForFLSevaluation.py
+++ english/src/ForFLSevaluation.py
@@ -3,7 +3,8 @@
 from collections import defaultdict
 import os
 import csv
-
+def str2id(s):
+    return str(s.__hash__()).replace('-','')
 def sort_by_value(d):
     items = d.items()
     backitems = [[v[1] , v[0]] for v in items]
@@ -27,7 +28,7 @@ if 'flseva' not in os.listdir('../data'):
 
 ####################################################
 userfls2annofls = defaultdict(lambda:'')
-for l in loadcsv('../data/csv/task6.csv'):
+for l in loadcsv('../data/csv/task3.csv'):
     userfls2annofls[l[1]] = l[3]
 
 ####################################################
@@ -38,7 +39,6 @@ iprob = open('../data/flseva/imine.Iprob','w')
 for l in open('../data/temp/flsposs.txt').readlines():
     segs=  l.strip().split('\t')
     annofls = segs[1]
-    id = segs[0]
     if segs[0] != id:
         count = 1
         id = segs[0]
@@ -49,54 +49,37 @@ for l in open('../data/temp/flsposs.txt').readlines():
 for k in annofls2idx:
     print k,annofls2idx[k]
 ######################################################
-run1 = open('../data/flseva/run1.sh','w')
-runlist = open('../data/flseva/iminerunlist','w')
-run2 = open('../data/flseva/run2.sh','w')
-run2.write('cat ./iminerunlist | ./TRECsplitruns ./imine.Iprob.tid')
-run2.close()
 
-run3 = open('../data/flseva/run3.sh','w')
-
-
+allsubfls = defaultdict(lambda:set())
+dqrels = open('../data/flseva/imine.Dqrels','w')
 for f in os.listdir('../data/enrun'):
-    run1.write('./DIN-splitqrels imine.Iprob '+f.replace('txt','Dqrels')+' imine'+'\n')
-    runlist.write(f.replace('.txt','')+'\n')
-    run3.write('echo '+f.replace('.txt','')+' | ./D-NTCIR-eval imine.Iprob.tid imine 5 100\n')
     print f
-    dreq = open('../data/flseva/'+f.replace('txt','Dqrels'),'w')
-    alreadyin = set()
-    pairlist = list()
+
     for l in open('../data/enrun/'+f).readlines()[1:]:
         segs = l.strip().replace('&amp;','').split(';')
         id = segs[0]
         userfls = segs[2]
         flsrank = int(segs[3])
-        if (id,userfls,flsrank) not in alreadyin:
-            alreadyin.add((id,userfls,flsrank))
-            pairlist.append((id,userfls,flsrank))
+        allsubfls[id].add(userfls)
     
-    for p in pairlist :
-        id = p[0]
-        if id in evaid():
-            
-            userfls = p[1]
-            flsrank = p[2]
-            annofls = userfls2annofls[userfls]
-#             print id,userfls,flsrank,annofls
-
-            if annofls !='':
+    for id in evaid():
+        for ufls in allsubfls[id]:
+            annofls = userfls2annofls[ufls]
+            if annofls != '':
                 try:
                     intent = annofls2idx[(id,annofls)]
                 except:
                     print '-------except-------'
-                    print userfls
+                    print id
+                    print ufls
                     print annofls
                     bug = open('../data/out.debug','w')
                     bug.write('fail key '+annofls+'\n')
                     for item in annofls2idx.keys():
                         bug.write(item[0]+'\t'+item[1]+'\n')
                     bug.close()
-                intent = 0
+                    intent = 0
+                    
                 if intent != 0:
                     rel = 'L1'
                 else:
@@ -104,9 +87,28 @@ for f in os.listdir('../data/enrun'):
             else:
                 intent = 0
                 rel = 'L0'
-            dreq.write(id+' '+str(intent)+' '+userfls.replace(' ','')+' '+rel+'\n')
-    dreq.close()
+            dqrels.write(id+' '+str(intent)+' '+str2id(ufls)+' '+rel+'\n')
 
-run1.close()
-run3.close()
-runlist.close()
+
+#############################################################################
+for f in os.listdir('../data/enrun'):
+    runout = open('../data/flseva/'+f.replace('txt','run'),'w')
+    alreadyin = set()
+    allsubfls = defaultdict(lambda:list())
+
+    for l in open('../data/enrun/'+f).readlines()[1:]:
+        segs = l.strip().replace('&amp;','').split(';')
+        id = segs[0]
+        userfls = segs[2]
+        userflsrank = segs[3]
+        userflsscore = segs[4]
+        if (userfls,userflsrank,userflsscore) in alreadyin:
+            continue
+        else:
+            alreadyin.add((userfls,userflsrank,userflsscore))
+            allsubfls[id].append((userfls,userflsrank,userflsscore))
+        
+    for id in evaid():
+        for sub in allsubfls[id]:
+            runout.write(id+' Q0 '+str2id(sub[0])+' '+sub[1]+' '+sub[2]+' '+f.replace('.txt','')+'\n')
+    runout.close()
